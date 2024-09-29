1a71ce81e7a862f6db90834e53c548df86cbb70b
Pallav kishor <pallavkishor01@gmail.com>
2024-09-29 17:01:18 +0530

    shows more about the website on click of learn more about us button

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,35 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 50 ? header.classList.add("active")
     : header.classList.remove("active");
 });
+
+/**
+  learn more about us 
+ **/
+const learnBtn=document.querySelector("#second-btn");
+const learnSection=document.querySelector(".section-lmas");
+learnBtn.addEventListener('click',()=>{
+  if(learnSection.style.display==='none'){
+    learnSection.style.display='block';
+  }
+  else{
+    learnSection.style.display='none';
+  }
+});
+
+const firstBtn=document.querySelector("#first-btn");
+const secondBtn=document.querySelector("#second-btn");
+firstBtn.addEventListener('click', () => {
+  secondBtn.scrollIntoView({ behavior: 'smooth' });
+});
+
+const testiBtn=document.querySelector(".testimonial-btn");
+const testiSection=document.querySelector(".testi");
+testiBtn.addEventListener('click', () => {
+  testiSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+const eventBtn=document.querySelector(".event-btn");
+const eventSection=document.getElementById('event');
+eventBtn.addEventListener('click', () => {
+  eventSection.scrollIntoView({ behavior: 'smooth' });
+});
