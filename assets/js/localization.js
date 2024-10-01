e4d688a4eeb19c28d22312d9ad7a4168fd56499f
Miron Barykin <barykinmiron@gmail.com>
2024-10-01 14:50:41 +0200

    German, Russian Languages Localization
    Script localization.js is created in assets/js
    - use data-localization="" on html tag in order to specify text that is supposed to be used
    - add the data-localization string in languagesData under the required language in order to load the content on language changes
    
    additionaly some styles were changed to satisfy German and Russian translations
    
    TODO:
    - Use user location to show appropriate language
    - Save user language in local settings, to avoid requirement to change it on any reload

---
+++ assets/js/localization.js
@@ -0,0 +1,283 @@
+const languagesData = {
+    english: {
+        nav_home: "Home",
+        nav_about: "About",
+        nav_service: "Service",
+        nav_donate: "Donate",
+        nav_event: "Events",
+        nav_contact: "Contact",
+        nav_search: "Search",
+        donation_button: "Donate",
+
+        home_welcome: "Welcome to WildGuard",
+        home_title: "Give Love for Saving <strong>World Animals</strong>",
+        home_moto: "Defending Wildlife, One Paw at a Time",
+        home_shelter_h: "Safe Shelter",
+        home_shelter_p: undefined,
+        home_water_h: "Clean Water",
+        home_water_p: undefined,
+        home_ecology_h: "Ecology",
+        home_ecology_p: undefined,
+        home_environment_h: "Environment",
+        home_environment_p: undefined,
+
+        about_deco: "About Us",
+        about_why_h: "Why Choose Us",
+        about_why_p: "Raise Your Hand to Save <strong>World Animals Life</strong>",
+        about_why_mission_h: "Our Mission",
+        about_why_mission_p: undefined,
+        about_why_vision_h: "Our Vision",
+        about_why_vision_p: undefined,
+        about_why_plan_h: "Our Plan",
+        about_why_plan_p: undefined,
+        about_charity_food: "Charity for Food",
+        about_charity_education: "Charity for Education",
+        about_charity_water: "Charity for Water",
+        about_charity_medical: "Charity for Medical",
+        about_more: "Learn More",
+
+        partners: "324+ Trusted Global Partners",
+        become_partner: "Become a Partner",
+
+        service_h: "What We Do",
+        service_p: "We Work Differently to <strong>Keep The World Safe</strong>",
+        service_card_nature_h: "Save Nature",
+        service_card_nature_p: undefined,
+        service_card_ecology_h: "Save Ecology",
+        service_card_ecology_p: undefined,
+        service_card_trees_h: "Tree Plantation",
+        service_card_trees_p: undefined,
+        service_card_ocean_h: "Clear Ocean",
+        service_card_ocean_p: undefined,
+        read_more: "Read More",
+
+        donate_hand: "Raise Hand To Save Animals",
+        donate_goal: "Goal",
+        donate_raised: "Raised",
+        donate_togo: "To Go",
+        donate_action: "Donate",
+
+        testimonials_h: "Our Testimonials",
+        testimonials_p: "What People Say About\n<strong>Our Organization</strong>",
+        testimonial_content: undefined,
+
+        events_h: "Events & Program",
+        events_p: "Our Most Popular Causes for <strong>Building Fund</strong>",
+        events_month_1: "Jan",
+        events_month_2: "Feb",
+        events_month_3: "Mar",
+        events_month_4: "Apr",
+        events_month_5: "May",
+        events_month_6: "Jun",
+        events_month_7: "Jul",
+        events_month_8: "Aug",
+        events_month_9: "Sep",
+        events_month_10: "Oct",
+        events_month_11: "Nov",
+        events_month_12: "Dec",
+        events_view: "View Event",
+
+        event_1_i: "Ocean Water",
+        event_1_h: "Far from the countries Vokalia and Consonantia 2022",
+        event_1_p: undefined,
+
+        legal_tou: "Terms of use",
+        legal_pp: "Privacy & Policy",
+        legal_rights: "All Rights Reserved",
+    },
+    german: {
+        nav_home: "Startseite",
+        nav_about: "Über uns",
+        nav_service: "Dienstleistungen",
+        nav_donate: "Spenden",
+        nav_event: "Veranstaltungen",
+        nav_contact: "Kontakt",
+        nav_search: "Suche",
+        donation_button: "Spenden",
+
+        home_welcome: "Willkommen bei WildGuard",
+        home_title: "Verschenken Sie Liebe für die Rettung der <strong>Tiere auf der Welt</strong>",
+        home_moto: "Schutz der Tierwelt, Pfote für Pfote",
+        home_shelter_h: "Sicherer Unterschlupf",
+        home_shelter_p: undefined,
+        home_water_h: "Sicheres Wasser",
+        home_water_p: undefined,
+        home_ecology_h: "Ökologie",
+        home_ecology_p: undefined,
+        home_environment_h: "Environment",
+        home_environment_p: undefined,
+
+        about_deco: "Über Uns",
+        about_why_h: "Warum Wir",
+        about_why_p: "Heben Sie Ihre Hand und retten Sie <strong>das Leben der Tiere auf der Welt</strong>",
+        about_why_mission_h: "Unsere Mission",
+        about_why_mission_p: undefined,
+        about_why_vision_h: "Unsere Vorstellung",
+        about_why_vision_p: undefined,
+        about_why_plan_h: "Unser Plan",
+        about_why_plan_p: undefined,
+        about_charity_food: "Spenden für Nahrung",
+        about_charity_education: "Spenden für Bildung",
+        about_charity_water: "Spenden für Wasser",
+        about_charity_medical: "Spenden für Medikamente",
+        about_more: "Mehr Erfahren",
+        
+        partners: "324+ Globale Partner",
+        become_partner: "Partner Werden",
+
+        service_h: "Was Machen Wir",
+        service_p: "Wir Arbeiten Anders, um <strong>die Welt zu Sichern</strong>",
+        service_card_nature_h: "Rette die Natur",
+        service_card_nature_p: undefined,
+        service_card_ecology_h: "Rette die Ökologie",
+        service_card_ecology_p: undefined,
+        service_card_trees_h: "Baumpflanzung",
+        service_card_trees_p: undefined,
+        service_card_ocean_h: "Säubern die Ozeane",
+        service_card_ocean_p: undefined,
+        read_more: "Mehr Lesen",
+
+        donate_hand: "Rette die Tiere",
+        donate_goal: "Ziel",
+        donate_raised: "Gesammelt",
+        donate_togo: "Geblieben",
+        donate_action: "Spenden",
+        
+        testimonials_h: "Unsere Bewertungen",
+        testimonials_p: "Was Sagen die Leute über\n<strong>Unsere Organisation</strong>",
+        testimonial_content: undefined,
+
+        events_h: "Veranstaltungen & Programm",
+        events_p: "Unsere beliebtesten Anliegen für den<strong>Aufbaufonds/strong>",
+
+        events_month_1: "Jan",
+        events_month_2: "Feb",
+        events_month_3: "Mär",
+        events_month_4: "Apr",
+        events_month_5: "Mai",
+        events_month_6: "Jun",
+        events_month_7: "Jul",
+        events_month_8: "Aug",
+        events_month_9: "Sep",
+        events_month_10: "Okt",
+        events_month_11: "Nov",
+        events_month_12: "Dez",
+        events_view: "Event Anschauen",
+
+        event_1_i: "Meereswasser",
+        event_1_h: "Fern der Länder Vokalia und Consonantia 2022",
+        event_1_p: undefined,
+
+        
+        legal_tou: "Nutzungsbedingungen",
+        legal_pp: "Datenschutz und Richtlinien",
+        legal_rights: "Alle Rechte vorbehalten",
+    },
+    russian: {
+        nav_home: "Главная",
+        nav_about: "О нас",
+        nav_service: "Услуги",
+        nav_donate: "Пожертвовать",
+        nav_event: "Мероприятия",
+        nav_contact: "Контакты",
+        nav_search: "Поиск",
+        donation_button: "Пожертвовать",
+
+
+        home_welcome: "Добро пожаловать в WildGuard",
+        home_title: "Поделитесь любовью для спасения <strong>Вымирающих Животных</strong>",
+        home_moto: "Защищайте природу, шаг за шагом",
+        home_shelter_h: "Безопасное Убежище",
+        home_shelter_p: undefined,
+        home_water_h: "Чистая Вода",
+        home_water_p: undefined,
+        home_ecology_h: "Экология",
+        home_ecology_p: undefined,
+        home_environment_h: "Среда обитания",
+        home_environment_p: undefined,
+
+        about_deco: "О Нас",
+        about_why_h: "Почему Мы",
+        about_why_p: "Протяните Руку для Спасения <strong>Жизни Животных</strong>",
+        about_why_mission_h: "Наша Миссия",
+        about_why_mission_p: undefined,
+        about_why_vision_h: "Наше Видение",
+        about_why_vision_p: undefined,
+        about_why_plan_h: "Наш План",
+        about_why_plan_p: undefined,
+        about_charity_food: "Сборы на Еду",
+        about_charity_education: "Сборы на Образование",
+        about_charity_water: "Сборы на Воду",
+        about_charity_medical: "Сборы на Медикаменты",
+        about_more: "Узнать Больше",
+
+        partners: "324+ Мировых Партнера",
+        become_partner: "Стать Партнером",
+
+        service_h: "Что Мы Делаем",
+        service_p: "Мы работаем по другом, для <strong>Защиты Природы</strong>",
+        service_card_nature_h: "Спасите Природу",
+        service_card_nature_p: undefined,
+        service_card_ecology_h: "Спасите Экологию",
+        service_card_ecology_p: undefined,
+        service_card_trees_h: "Высадка Деревьев",
+        service_card_trees_p: undefined,
+        service_card_ocean_h: "Отчистка Океанов",
+        service_card_ocean_p: undefined,
+        read_more: "Читать Больше",
+
+        donate_hand: "Спаси Животных",
+        donate_goal: "Цель",
+        donate_raised: "Собрано",
+        donate_togo: "Осталось",
+        donate_action: "Пожертвовать",
+
+        testimonials_h: "Наши Отзывы",
+        testimonials_p: "Что Говорят Люди О\n<strong>Нашей Организации</strong>",
+        testimonial_content: undefined,
+
+        events_h: "События & Программа",
+        events_p: "Популярные Мероприятия для <strong>Сбора Пожертвований</strong>",
+
+        events_month_1: "Янв",
+        events_month_2: "Фев",
+        events_month_3: "Мар",
+        events_month_4: "Апр",
+        events_month_5: "Май",
+        events_month_6: "Июн",
+        events_month_7: "Июл",
+        events_month_8: "Авг",
+        events_month_9: "Сен",
+        events_month_10: "Окт",
+        events_month_11: "Ноя",
+        events_month_12: "Дек",
+        events_view: "Посмотреть Мероприятие",
+
+        event_1_i: "Океанические Воды",
+        event_1_h: "В далеке от Вокалии и Конзонантии 2022",
+        event_1_p: undefined,
+
+        legal_tou: "Условия пользования",
+        legal_pp: "Политика конфиденциальности",
+        legal_rights: "Все права защищены",
+    }
+};
+
+const languageSelector = document.getElementById('language-select');
+const translatableElements = document.querySelectorAll('[data-localized]');
+
+function updateLanguage(language) {
+    translatableElements.forEach(element => {
+        const key = element.getAttribute('data-localized');
+        if (languagesData[language][key]) {
+            element.innerHTML = languagesData[language][key];
+        }
+    });
+}
+
+languageSelector.addEventListener('change', function() {
+    const selectedLanguage = this.value;
+    updateLanguage(selectedLanguage);
+});
+
+updateLanguage('english');
