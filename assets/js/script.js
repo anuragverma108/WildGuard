c8c6bac88e8578d61f7dbb38eb6b86465ada48ae
diwakarshukla941 <shukladiwakar941@gmail.com>
2024-10-08 19:18:20 +0530

    scroll to top btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,32 +104,32 @@ function next_plan(){
 
 
 // Scroll To Top Button
-// Get the button
-// let mybutton = document.getElementById("btn-back-to-top");
+Get the button
+let mybutton = document.getElementById("btn-back-to-top");
 
-// // Show button on scroll
-// window.onscroll = function () {
-//     scrollFunction();
-// };
+// Show button on scroll
+window.onscroll = function () {
+    scrollFunction();
+};
 
-// function scrollFunction() {
-//     if (
-//         document.body.scrollTop > 20 ||
-//         document.documentElement.scrollTop > 20
-//     ) {
-//         mybutton.style.display = "block";
-//     } else {
-//         mybutton.style.display = "none";
-//     }
-// }
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
 
-// // Scroll to top
-// mybutton.addEventListener("click", backToTop);
+// Scroll to top
+mybutton.addEventListener("click", backToTop);
 
-// function backToTop() {
-//     document.body.scrollTop = 0; // For Safari
-//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
+function backToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 /**
 *search-bar with functionality
